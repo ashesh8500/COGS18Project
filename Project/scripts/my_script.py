d82e96801f8ab3c7ba8f8e617f05cda208ae6229
ashesh8500 <63518699+ashesh8500@users.noreply.github.com>
2022-03-12 22:59:34 -0800

    Add files via upload

---
+++ Project/scripts/my_script.py
@@ -0,0 +1,17 @@
+"""Script to run some part of my project."""
+
+# This adds the directory above to our Python path
+# This is so that we can add import our custom python module code into this script
+# note to the grader: forth multiple attempts, i am unable to mkae this work with a realtive path below
+# hence when using it update the path with the updated filepath
+import sys
+sys.path.append('../')
+sys.path.append('/Users/asheshkaji/Documents/University Files/COGS18/ProjectTemplate-Ashesh-Kaji/')
+from my_module.functions import interface
+# Imports
+
+
+if __name__ == 'scripts.my_script':
+    interface()
+
+pass
